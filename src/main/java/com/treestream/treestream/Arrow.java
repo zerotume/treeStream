eb0911be0580ec6901f1929b674ecf38edaa3bf3
zerotume@gmail.com <zerotume@gmail.com>
2024-09-30 22:36:34 -0400

    arrow class

--- src/main/java/com/treestream/treestream/Arrow.java
+++ src/main/java/com/treestream/treestream/Arrow.java
@@ -1,4 +1,98 @@
 package com.treestream.treestream;
 
-public class Arrow {
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleBinding;
+import javafx.scene.Group;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+
+public class Arrow extends Group {
+
+    private Line line;
+    private Polygon arrowHead;
+    private DraggableNodeController sourceNode;
+    private DraggableNodeController targetNode;
+    private boolean isSelected = false;
+
+    public Arrow(DraggableNodeController sourceNode, DraggableNodeController targetNode) {
+        this.sourceNode = sourceNode;
+        this.targetNode = targetNode;
+
+        line = new Line();
+        arrowHead = new Polygon();
+        arrowHead.getPoints().addAll(0.0, 0.0, -10.0, -5.0, -10.0, 5.0);
+
+        line.setStrokeWidth(2);
+        line.setStroke(Color.BLACK);
+
+        arrowHead.setFill(Color.BLACK);
+
+        // Bind line start and end points to node positions
+        line.startXProperty().bind(Bindings.createDoubleBinding(
+                () -> sourceNode.getLayoutX() + sourceNode.getWidth() / 2,
+                sourceNode.layoutXProperty(), sourceNode.widthProperty()));
+
+        line.startYProperty().bind(Bindings.createDoubleBinding(
+                () -> sourceNode.getLayoutY() + sourceNode.getHeight() / 2,
+                sourceNode.layoutYProperty(), sourceNode.heightProperty()));
+
+        line.endXProperty().bind(Bindings.createDoubleBinding(
+                () -> targetNode.getLayoutX() + targetNode.getWidth() / 2,
+                targetNode.layoutXProperty(), targetNode.widthProperty()));
+
+        line.endYProperty().bind(Bindings.createDoubleBinding(
+                () -> targetNode.getLayoutY() + targetNode.getHeight() / 2,
+                targetNode.layoutYProperty(), targetNode.heightProperty()));
+
+        // Position arrowhead
+        arrowHead.layoutXProperty().bind(line.endXProperty());
+        arrowHead.layoutYProperty().bind(line.endYProperty());
+
+        // Rotate arrowhead to point along the line
+        DoubleBinding angle = Bindings.createDoubleBinding(() -> {
+            double deltaX = line.getEndX() - line.getStartX();
+            double deltaY = line.getEndY() - line.getStartY();
+            return Math.atan2(deltaY, deltaX) * 180 / Math.PI;
+        }, line.startXProperty(), line.startYProperty(), line.endXProperty(), line.endYProperty());
+
+        arrowHead.rotateProperty().bind(angle);
+
+        this.getChildren().addAll(line, arrowHead);
+
+        addSelectionHandler();
+    }
+
+    private void addSelectionHandler() {
+        this.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 1) {
+                setSelected(true);
+                event.consume();
+            }
+        });
+    }
+
+    public void setSelected(boolean select) {
+        isSelected = select;
+        if (isSelected) {
+            line.setStroke(Color.BLUE);
+            arrowHead.setFill(Color.BLUE);
+        } else {
+            line.setStroke(Color.BLACK);
+            arrowHead.setFill(Color.BLACK);
+        }
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public DraggableNodeController getSourceNode() {
+        return sourceNode;
+    }
+
+    public DraggableNodeController getTargetNode() {
+        return targetNode;
+    }
 }
