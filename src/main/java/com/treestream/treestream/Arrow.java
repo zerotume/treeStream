22623744f458265890a5cbeaf581799bcf1652b9
zerotume@gmail.com <zerotume@gmail.com>
2024-10-03 22:43:05 -0400

    caught the issue in arrow

--- src/main/java/com/treestream/treestream/Arrow.java
+++ src/main/java/com/treestream/treestream/Arrow.java
@@ -14,43 +14,68 @@ public class Arrow extends Group {
     private Polygon arrowHead;
     private DraggableNodeController sourceNode;
     private DraggableNodeController targetNode;
-    private boolean isSelected = false;
-
     private MainController mainController;
+    private boolean isSelected = false;
 
     public Arrow(MainController mainController, DraggableNodeController sourceNode, DraggableNodeController targetNode) {
         this.mainController = mainController;
-
         this.sourceNode = sourceNode;
         this.targetNode = targetNode;
 
         line = new Line();
         arrowHead = new Polygon();
-        arrowHead.getPoints().addAll(0.0, 0.0, -10.0, -5.0, -10.0, 5.0);
+        arrowHead.getPoints().addAll(
+                0.0, 0.0,
+                -20.0, -15.0, // Increased size
+                -20.0, 15.0
+        );
 
         line.setStrokeWidth(2);
         line.setStroke(Color.BLACK);
 
         arrowHead.setFill(Color.BLACK);
+        arrowHead.setStroke(Color.BLACK);
+        arrowHead.setStrokeWidth(1);
 
         // Bind line start and end points to node positions
-        line.startXProperty().bind(Bindings.createDoubleBinding(
-                () -> sourceNode.getLayoutX() + sourceNode.getWidth() / 2,
+        updateLineBindings();
+
+        // Position and rotate arrowhead
+        updateArrowheadBindings();
+
+        this.getChildren().addAll(line, arrowHead);
+
+        addSelectionHandler();
+
+//        line.endXProperty().addListener((obs, oldVal, newVal) -> {
+//            System.out.println("Line endX: " + newVal);
+//        });
+//
+//        arrowHead.layoutXProperty().addListener((obs, oldVal, newVal) -> {
+//            System.out.println("arrow endX: " + newVal);
+//        });
+    }
+
+    private void updateLineBindings() {
+        line.startXProperty().bind(Bindings.createDoubleBinding(() ->
+                        sourceNode.getLayoutX() + sourceNode.getWidth() / 2,
                 sourceNode.layoutXProperty(), sourceNode.widthProperty()));
 
-        line.startYProperty().bind(Bindings.createDoubleBinding(
-                () -> sourceNode.getLayoutY() + sourceNode.getHeight() / 2,
+        line.startYProperty().bind(Bindings.createDoubleBinding(() ->
+                        sourceNode.getLayoutY() + sourceNode.getHeight() / 2,
                 sourceNode.layoutYProperty(), sourceNode.heightProperty()));
 
-        line.endXProperty().bind(Bindings.createDoubleBinding(
-                () -> targetNode.getLayoutX() + targetNode.getWidth() / 2,
+        line.endXProperty().bind(Bindings.createDoubleBinding(() ->
+                        targetNode.getLayoutX() + targetNode.getWidth() / 2,
                 targetNode.layoutXProperty(), targetNode.widthProperty()));
 
-        line.endYProperty().bind(Bindings.createDoubleBinding(
-                () -> targetNode.getLayoutY() + targetNode.getHeight() / 2,
+        line.endYProperty().bind(Bindings.createDoubleBinding(() ->
+                        targetNode.getLayoutY() + targetNode.getHeight() / 2,
                 targetNode.layoutYProperty(), targetNode.heightProperty()));
+    }
 
-        // Position arrowhead
+    private void updateArrowheadBindings() {
+        // Position arrowhead at the end of the line
         arrowHead.layoutXProperty().bind(line.endXProperty());
         arrowHead.layoutYProperty().bind(line.endYProperty());
 
@@ -58,14 +83,10 @@ public class Arrow extends Group {
         DoubleBinding angle = Bindings.createDoubleBinding(() -> {
             double deltaX = line.getEndX() - line.getStartX();
             double deltaY = line.getEndY() - line.getStartY();
-            return Math.atan2(deltaY, deltaX) * 180 / Math.PI;
+            return Math.toDegrees(Math.atan2(deltaY, deltaX));
         }, line.startXProperty(), line.startYProperty(), line.endXProperty(), line.endYProperty());
 
         arrowHead.rotateProperty().bind(angle);
-
-        this.getChildren().addAll(line, arrowHead);
-
-        addSelectionHandler();
     }
 
     private void addSelectionHandler() {
@@ -79,15 +100,16 @@ public class Arrow extends Group {
         });
     }
 
-
     public void setSelected(boolean select) {
         isSelected = select;
         if (isSelected) {
             line.setStroke(Color.BLUE);
             arrowHead.setFill(Color.BLUE);
+            arrowHead.setStroke(Color.BLUE);
         } else {
             line.setStroke(Color.BLACK);
             arrowHead.setFill(Color.BLACK);
+            arrowHead.setStroke(Color.BLACK);
         }
     }
 
